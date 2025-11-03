8943ea9378267cff6da5d3e4be4129d94e0245e2
Navodit <navoditsharma09@gmail.com>
2025-11-03 20:06:35 +0530

    Initial commit - setup frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 8080;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend server is running for CollabVerse 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
